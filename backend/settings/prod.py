5f708e7f38c3481c55e3766c9389d99af0eba092
peterwade153 <peterwade153@gmail.com>
2024-01-23 20:10:42 +0300

    update settings in preping for deployment

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -1,10 +1,12 @@
+import os
 import dj_database_url
+
 from .base import *
 
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=True)
-
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
