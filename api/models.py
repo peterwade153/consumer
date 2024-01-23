88745008395ad92ad9ef8c57c676a51ddde54a0a
peterwade153 <peterwade153@gmail.com>
2024-01-23 18:57:47 +0300

    Add consumer model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.gis.db.models import PointField
 
-# Create your models here.
+
+class Consumer(models.Model):
+    consumer_id = models.IntegerField(unique=True)
+    street = models.CharField(max_length=50)
+    status = models.CharField(max_length=20)
+    previous_jobs_count = models.IntegerField()
+    amount_due = models.IntegerField()
+    geometry = PointField()
+
+    def __str__(self):
+        return self.consumer_id
