c3e7779d2f3f6894e9ba3e9492878b66348e6284
peterwade153 <peterwade153@gmail.com>
2024-01-23 19:00:38 +0300

    update settings to allow multiple envrionment support

---
+++ backend/settings/local.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
---
+++ backend/settings/prod.py
@@ -0,0 +1,10 @@
+import dj_database_url
+from .base import *
+
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=500, ssl_require=True)
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
