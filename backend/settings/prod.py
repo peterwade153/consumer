146c28b78418620be391d6a93f1ff0575f2505f5
peterwade153 <peterwade153@gmail.com>
2024-01-23 21:08:52 +0300

    update prod settings to add postgis engine

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -5,7 +5,11 @@ from .base import *
 
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    "default": dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'), 
+        engine='django.contrib.gis.db.backends.postgis',
+        conn_max_age=600
+    ),
 }
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
